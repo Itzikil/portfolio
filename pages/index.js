1ef66bdc04495953f1e3ac25a610a88e62785771
itzhake <itzikil@icloud.com>
2023-01-24 15:17:12 +0200

    4

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import Head from 'next/head'
-import { Inter } from '@next/font/google'
 
 import { BsFillMoonStarsFill } from 'react-icons/bs'
 import { AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai'
-import Image from 'next/image'
 import deved from '../public/dev-ed-wave.png'
 import design from '../public/design.png'
 import code from '../public/code.png'
 import consulting from '../public/consulting.png'
+import Image from 'next/legacy/image'
 import web1 from '../public/web1.png'
 import web2 from '../public/web2.png'
 import web3 from '../public/web3.png'
@@ -16,10 +15,10 @@ import web5 from '../public/web5.png'
 import web6 from '../public/web6.png'
 import { useState } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState()
+
   return (
     <div className={darkMode ? 'dark' : ''}>
       <Head>
@@ -31,8 +30,8 @@ export default function Home() {
 
       <main className='bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900'>
         <section className='min-h-screen'>
-          <nav className='py-10 mb-12 flex justify-between'>
-            <h1 className='text-xl font-burtons dark:text-gray-200'>developedbyed</h1>
+          <nav className='py-10 mb-12 flex justify-between dark:text-white'>
+            <h1 className='text-xl font-burtons dark:text-gray-200'>developedbyil</h1>
             <ul className='flex items-center'>
               <li><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-xl dark:text-gray-200' /></li>
               <li>
@@ -40,7 +39,7 @@ export default function Home() {
               </li>
             </ul>
           </nav>
-          <div className='text-center p-10'>
+          <div className='text-center p-10 py-10'>
             <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl dark:text-teal-400 '>Issac Levi</h2>
             <h3 className='text-2xl py-2 md:text-3xl  dark:text-white'>Fullstack Developer</h3>
             <p className='text-md py-5 leading-8 text-gray-800 max-w-xl mx-auto md:text-xl  dark:text-gray-200'>
@@ -53,7 +52,7 @@ export default function Home() {
             <AiFillLinkedin />
             <AiFillYoutube />
           </div>
-          <div className='relative  mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96'>
+          <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96'>
             <Image src={deved} layout="fill" objectFit='cover' overflow />
           </div>
         </section>
@@ -75,7 +74,7 @@ export default function Home() {
             </p>
           </div>
           <div className='lg:flex gap-10'>
-            <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white'>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1'>
               <Image src={design} width={100} height={100} />
               <h3 className='text-lg font-medium pt-8 pb-2'>Beatiful Designs</h3>
               <p className="py-2">
@@ -88,7 +87,7 @@ export default function Home() {
               <p className="text-gray-800 py-1">Figma</p>
               <p className="text-gray-800 py-1">Indesign</p>
             </div>
-            <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white'>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1'>
               <Image src={code} width={100} height={100} />
               <h3 className='text-lg font-medium pt-8 pb-2'>Beatiful Designs</h3>
               <p className="py-2">
@@ -101,7 +100,7 @@ export default function Home() {
               <p className="text-gray-800 py-1">Figma</p>
               <p className="text-gray-800 py-1">Indesign</p>
             </div>
-            <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white'>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1'>
               <Image src={consulting} width={100} height={100} />
               <h3 className='text-lg font-medium pt-8 pb-2'>Beatiful Designs</h3>
               <p className="py-2">
